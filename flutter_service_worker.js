8f751df53e035ab55a71e1166efe9211e331f954
jun_mac <junwatanabe72@gmail.com>
2021-03-12 10:13:59 +0900

    seventh

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "adview.html": "2699f17145e9c11d7349b7ed3a79a016",
 "version.json": "714ff95f27498b2ce9c6980e5e409460",
-"index.html": "f8fd75f6270b87410932c2691359436f",
-"/": "f8fd75f6270b87410932c2691359436f",
-"main.dart.js": "174565954a13b3e88342d85db6505872",
+"index.html": "c4c09a40963274d9d8c57bd71d5efebf",
+"/": "c4c09a40963274d9d8c57bd71d5efebf",
+"main.dart.js": "1f5d975c58c76467c0bf79abfbc25a4b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/kokki_clicker_219.png": "f26d7d6556eb8d8776e47848b03b739a",
